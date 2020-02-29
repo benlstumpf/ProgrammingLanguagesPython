a2d4fd7bd8450ebd24c9b034adfe231d397a196f
Ben Stumpf <benlstumpf@gmail.com>
2020-02-29 17:07:21 -0600

    biult constructor and setter function

--- PlaneClass.py
+++ PlaneClass.py
@@ -4,8 +4,20 @@
 #given airlines at
 
 
-def main():
+class Plane(object):
+    """docstring for Plane."""
+    id = ""
+    submissionTime =
+    requestedStart =
+    requestedDuration =
+    timeFinished = requestedStart + requestedDuration
 
+    def __init__(self, id, submissionTime, requestedStart, requestedDuration):
+        super(Plane, self).__init__()
+        self.id = id
+        self.submissionTime = submissionTime
+        self.requestedStart = requestedStart
+        self.requestedDuration = requestedDuration
 
-if __name__ == "__main__"
-    main()
+    def setTimeFinished(self, time)
+        self.timeFinished = time
