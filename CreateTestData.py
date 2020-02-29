bc0a45d73e0b962f7d77280240429043562fd554
Ben Stumpf <benlstumpf@gmail.com>
2020-02-29 14:43:38 -0600

    init data creation program

---
+++ CreateTestData.py
@@ -0,0 +1,9 @@
+#Test Data Creation program
+#MSP airlines are Aer Lingus, Air Canada, Air Choice One, Air France, Alaska Airlines, American Airlines, Boutique Air, Delta Air Lines, Frontier Airlines, KLM Royal Dutch Airlines, Spirit Airlines, United Airlines
+
+
+
+def main():
+
+if __name__ == "__main__"
+    main()
