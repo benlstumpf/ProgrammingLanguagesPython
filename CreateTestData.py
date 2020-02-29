d735921bcf1d1c7b93d7ca258881360e2a4d2814
Ben Stumpf <benlstumpf@gmail.com>
2020-02-29 15:13:30 -0600

    defined getID

--- CreateTestData.py
+++ CreateTestData.py
@@ -6,10 +6,31 @@
 import random
 
 def main():
-    while i in range sys.argv[0]:
-        id = getID
+    passedArgument = sys.argv[0]
+    while i in range passedArgument:
+        if i%
+
+def createPlane():
+    id = getID
 
 def getID():
+     # 12 is the number of airlines listed at start of file
+     # started counting at 1 and realized it at the end so united is at the top
+    return {
+        "United Airlines" : 0,
+        "Aer Lingus" : 1,
+        "Air Canada" : 2,
+        "Air Choice One" : 3,
+        "Air France" : 4,
+        "Alaska Airlines" : 5,
+        "American Airlines" : 6,
+        "Boutique Air" : 7,
+        "Delta Air Lines" : 8,
+        "Frontier Airlines" : 9,
+        "KLM Royal Dutch Airlines" : 10,
+        "Spirit Airlines" : 11
+    }.get(random.randrange( 12)
+
 
 if __name__ == "__main__"
     main()
