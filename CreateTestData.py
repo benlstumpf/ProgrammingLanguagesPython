28bfa7ac4438d3423485c91086849d0f03e886ca
Ben Stumpf <benlstumpf@gmail.com>
2020-02-29 14:57:28 -0600

    while loop and get id init

--- CreateTestData.py
+++ CreateTestData.py
@@ -1,9 +1,15 @@
 #Test Data Creation program
 #MSP airlines are Aer Lingus, Air Canada, Air Choice One, Air France, Alaska Airlines, American Airlines, Boutique Air, Delta Air Lines, Frontier Airlines, KLM Royal Dutch Airlines, Spirit Airlines, United Airlines
 
+#format is ID, Submission Time, Requested Start, Requested Duration
 
+import random
 
 def main():
+    while i in range sys.argv[0]:
+        id = getID
+
+def getID():
 
 if __name__ == "__main__"
     main()
