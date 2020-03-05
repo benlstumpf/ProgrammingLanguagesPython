0d6150d71713bbe1866ec8782a63a2d90367418e
Ben Stumpf <benlstumpf@gmail.com>
2020-03-05 11:24:49 -0600

    initilizing variables

--- PlaneClass.py
+++ PlaneClass.py
@@ -7,10 +7,11 @@
 class Plane(object):
     """docstring for Plane."""
     id = ""
-    submissionTime =
-    requestedStart =
-    requestedDuration =
+    submissionTime = 0
+    requestedStart = 0
+    requestedDuration = 0
     realTimeStart = requestedStart + requestedDuration
+    scheduledTime = requestedStart
 
     def __init__(self, id, submissionTime, requestedStart, requestedDuration):
         super(Plane, self).__init__()
