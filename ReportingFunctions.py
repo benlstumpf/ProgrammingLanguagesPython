0467285b3fc07cb83e2711ecaeaa36315007f4f1
Ben Stumpf <benlstumpf@gmail.com>
2020-03-05 14:51:40 -0600

    defined printCurrentQueue

--- ReportingFunctions.py
+++ ReportingFunctions.py
@@ -1,1 +1,14 @@
 #this file will have reporting functions
+
+
+#At time 2 the queue would look like: Delta 160 (started at 0), Delta 6 (scheduled for 4), UAL 120 (scheduled for 10)
+
+
+def printCurrentQueue (planeQueue, time)
+    print("At time", time, "the queue would look like:", end=" ")
+    for index in planeQueue
+        if hasattr(index, 'realTimeStart'):
+            print(index.id, "(started at", index.realTimeStart, ")", end=" ")
+        else
+            print(index.id, "(scheduled for", index.scheduledTime, ")", end=" ")
+    print()
