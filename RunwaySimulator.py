ddbc791ae808093cac38e63a0fe93518a9a8ea97
Ben Stumpf <benlstumpf@gmail.com>
2020-03-07 16:39:22 -0600

    variable instatiation and isFinished defined

--- RunwaySimulator.py
+++ RunwaySimulator.py
@@ -1,1 +1,12 @@
 #This will contain the logic for the runway simulation
+
+
+def runway(submissionList):
+    currentQueue = []
+    finishedList = []
+
+def isFinished (submissionList, CurrentQueue):
+    if not submissionList && not CurrentQueue:
+        return True
+    else:
+        False
