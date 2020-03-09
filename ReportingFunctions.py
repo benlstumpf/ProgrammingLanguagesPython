3914949954ba29fbda5067141405ddb7807913c4
Ben Stumpf <benlstumpf@gmail.com>
2020-03-09 16:17:22 -0500

    forgot :'s

--- ReportingFunctions.py
+++ ReportingFunctions.py
@@ -4,21 +4,23 @@
 #At time 2 the queue would look like: Delta 160 (started at 0), Delta 6 (scheduled for 4), UAL 120 (scheduled for 10)
 
 #Delta 160 (0-3), Delta 6 (4-9), UAL 120 (10-13)
+
+import PlaneClass
+
 """
 docstring stub
 """
-def printCurrentQueue (planeQueue, time):
+def printCurrentQueue(planeQueue, time):
     print("At time", time, "the queue would look like:", end=" ")
-    for index in planeQueue
+    for index in planeQueue:
         if hasattr(index, 'realTimeStart'):
             print(index.id, "(started at", index.realTimeStart, ")", end=" ")
-        else
+        else:
             print(index.id, "(scheduled for", index.scheduledTime, ")", end=" ")
-    print()
-    # TODO: finish print statment
+
 """
 docstring stub
 """
 def printFinalPrint(planeQueue):
-    for index in planeQueue
+    for index in planeQueue:
         print(index.id, "(", index.setRealTimeStart, "-", index.requestedDuration, "),", end=" ")
